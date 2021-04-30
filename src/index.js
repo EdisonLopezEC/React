da7486bbea92025288c9b061c7850ee753c1bf4f
Edison <904edisonlopez@gmail.com>
2021-04-30 10:34:19 -0500

    FetchAPI

--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
-import getHeroesById from './data/gHeroe'
+const apiKey = '6Rj2C0QdylT0T0Oj00xuyI8LNqKEka8b';
 
-// console.log('empiez'); 
-// const promesa = new Promise( (resolve,reject)=> {
-//     setTimeout(()=>{
-//         const heroe=getHeroesById(3);
-//         reject(heroe);
-//         // reject('No se pudo encontrar el heroe');
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
 
-//     },2000);
-// });
-
-
-// promesa.then( (heroe)=> {
-//     console.log('Heroe: ',heroe);
-// })
-// .catch(err=>console.warn(err));
-
-const getHeroeByAsync = (id)=>{
-    const promesa = new Promise( (resolve,reject)=> {
-        setTimeout(()=>{
-            const heroe=getHeroesById(id);
-            if(heroe){
-                resolve(heroe);
-            }else{
-                reject('No se pudo encontrar el heroe');
-            }
-            // reject('No se pudo encontrar el heroe');
-    
-        },2000);
-    });
-
-    return promesa;
-}
-
-getHeroeByAsync(4)
-.then(console.log)
-.catch(console.warn);
-
-                
-
-
+peticion
+    .then(resp=>resp.json())
+    .then(({data})=>{
+        const { url }=data.images.original;
+        const img =document.createElement('img');
+        img.src=url;
+        document.body.append(img);
+    })
+    .catch(console.warn);
