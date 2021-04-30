fcb095cda69b428a7f10ad776e42e44d0bc374f9
Edison <904edisonlopez@gmail.com>
2021-04-30 00:38:07 -0500

    Promesa2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import getHeroesById from './data/gHeroe'
 
-console.log('empiez');
-const promesa = new Promise( (resolve,reject)=> {
-    setTimeout(()=>{
-        const heroe=getHeroesById(3);
-        console.log(heroe);
-    },2000);
-});
+// console.log('empiez'); 
+// const promesa = new Promise( (resolve,reject)=> {
+//     setTimeout(()=>{
+//         const heroe=getHeroesById(3);
+//         reject(heroe);
+//         // reject('No se pudo encontrar el heroe');
+
+//     },2000);
+// });
 
 
-promesa.then( ()=> {
-    console.log('Then de la promesa');
-})
+// promesa.then( (heroe)=> {
+//     console.log('Heroe: ',heroe);
+// })
+// .catch(err=>console.warn(err));
+
+const getHeroeByAsync = (id)=>{
+    const promesa = new Promise( (resolve,reject)=> {
+        setTimeout(()=>{
+            const heroe=getHeroesById(id);
+            if(heroe){
+                resolve(heroe);
+            }else{
+                reject('No se pudo encontrar el heroe');
+            }
+            // reject('No se pudo encontrar el heroe');
+    
+        },2000);
+    });
+
+    return promesa;
+}
+
+getHeroeByAsync(4)
+.then(console.log)
+.catch(console.warn);
+
+                
+
+
