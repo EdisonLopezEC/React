9d0032ff6ddf7773949753dfc3c77b2e06c94f3a
Edison <904edisonlopez@gmail.com>
2021-04-30 11:42:32 -0500

    Async-Await

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-const apiKey = '6Rj2C0QdylT0T0Oj00xuyI8LNqKEka8b';
 
-const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
 
-peticion
-    .then(resp=>resp.json())
-    .then(({data})=>{
-        const { url }=data.images.original;
-        const img =document.createElement('img');
-        img.src=url;
-        document.body.append(img);
-    })
-    .catch(console.warn);
+const getImagen = async()=>{
+
+    const apiKey = '6Rj2C0QdylT0T0Oj00xuyI8LNqKEka8b';
+    const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+    const { data } = await resp.json();
+    const { url }=data.images.original;
+    const img =document.createElement('img');
+    img.src=url;
+    document.body.append(img);
+}
+
+getImagen().then(console.log);
+
+
+
+
+// peticion
+//     .then(resp=>resp.json())
+//     .then(({data})=>{
+//         const { url }=data.images.original;
+//         const img =document.createElement('img');
+//         img.src=url;
+//         document.body.append(img);
+//     })
+//     .catch(console.warn);
